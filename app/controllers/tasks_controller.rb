90796a01b1cf1f79e0196bb67e3e8af1a3863f2b
Maria Micaela Vidal <131964698+mikividal@users.noreply.github.com>
2025-05-06 13:02:17 +0100

    adding new ok

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,24 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(require_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def require_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
