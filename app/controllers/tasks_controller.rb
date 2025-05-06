5b62a2597f58cfc7aa9b551c81e643244e9c35df
Maria Micaela Vidal <131964698+mikividal@users.noreply.github.com>
2025-05-06 14:39:59 +0100

    everything working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
@@ -18,7 +19,30 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(require_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
   def require_params
     params.require(:task).permit(:title, :details, :completed)
   end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
 end
